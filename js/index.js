6948858c08563e51304afa01e6d1f71e2c7eb808
carronatwood <63373697+carronatwood@users.noreply.github.com>
2020-10-31 22:51:25 -0500

    2nd Commit
    ready for feedback!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 console.log("ur js is loaded correctly, proud of u");
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navigationTop");
+var sticky = navbar.offsetTop;
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
